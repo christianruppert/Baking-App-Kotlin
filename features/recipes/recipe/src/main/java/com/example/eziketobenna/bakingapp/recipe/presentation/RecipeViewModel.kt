240daeed5029f8516b88099b7f851f1162e4962d
Christian Ruppert <ruppert.christian@googlemail.com>
2020-08-20 12:36:46 +0200

    Initialize Feature/presentation_tests

---
+++ features/recipes/recipe/src/main/java/com/example/eziketobenna/bakingapp/recipe/presentation/RecipeViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.eziketobenna.bakingapp.recipe.presentation
+
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eziketobenna.bakingapp.model.RecipeModel
+import com.example.eziketobenna.bakingapp.navigation.NavigationDispatcher
+import com.example.eziketobenna.bakingapp.presentation.mvi.MVIPresenter
+import com.example.eziketobenna.bakingapp.recipe.presentation.mvi.RecipeViewAction
+import com.example.eziketobenna.bakingapp.recipe.presentation.mvi.RecipeViewIntent
+import com.example.eziketobenna.bakingapp.recipe.presentation.mvi.RecipeViewState
+import javax.inject.Inject
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.scan
+
+class RecipeViewModel @Inject constructor(
+    private val recipeActionProcessor: RecipeViewActionProcessor,
+    private val recipeViewStateReducer: RecipeStateReducer,
+    private val recipeViewIntentProcessor: RecipeIntentProcessor,
+    private val navigationDispatcher: NavigationDispatcher
+) : ViewModel(), MVIPresenter<RecipeViewIntent, RecipeViewState> {
+
+    private val _recipeViewState: MutableStateFlow<RecipeViewState> =
+        MutableStateFlow(RecipeViewState.init)
+
+    override val viewState: StateFlow<RecipeViewState>
+        get() = _recipeViewState
+
+    @get:VisibleForTesting
+    val en = mutableSetOf<RecipeViewState>()
+
+    /** Using a channel cos [MutableStateFlow] doesn't emit subsequent values of the same type */
+    private val actionsChannel =
+        ConflatedBroadcastChannel<RecipeViewAction>(RecipeViewAction.LoadInitialAction)
+
+    init {
+        processActions()
+    }
+
+    override fun processIntent(intents: Flow<RecipeViewIntent>) {
+        intents.onEach { intent ->
+            actionsChannel.offer(recipeViewIntentProcessor.intentToAction(intent))
+        }.launchIn(viewModelScope)
+    }
+
+    private fun processActions() {
+        actionsChannel.asFlow()
+            .flatMapMerge { action ->
+                recipeActionProcessor.actionToResult(action)
+            }.scan(RecipeViewState.init) { previous, result ->
+                recipeViewStateReducer.reduce(previous, result)
+            }.distinctUntilChanged()
+            .onEach { recipeViewState ->
+                _recipeViewState.value = recipeViewState
+                en.add(recipeViewState)
+            }.launchIn(viewModelScope)
+    }
+
+    fun openRecipeDetail(model: RecipeModel) {
+        navigationDispatcher.openRecipeDetail(model)
+    }
+}
---
+++ features/recipes/stepDetail/src/main/java/com/example/eziketobenna/bakingapp/stepdetail/presentation/StepDetailViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.eziketobenna.bakingapp.stepdetail.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eziketobenna.bakingapp.navigation.NavigationDispatcher
+import com.example.eziketobenna.bakingapp.presentation.mvi.MVIPresenter
+import javax.inject.Inject
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.scan
+
+class StepDetailViewModel @Inject constructor(
+    private val stepDetailIntentProcessor: StepIntentProcessor,
+    private val stepDetailActionProcessor: StepActionProcessor,
+    private val viewStateReducer: StepViewStateReducer,
+    private val navigationDispatcher: NavigationDispatcher
+) : ViewModel(), MVIPresenter<StepDetailViewIntent, StepDetailViewState> {
+
+    private val stepDetailViewState: MutableStateFlow<StepDetailViewState> =
+        MutableStateFlow(StepDetailViewState.Idle)
+
+    private val actionsChannel =
+        ConflatedBroadcastChannel<StepDetailViewAction>(StepDetailViewAction.Idle)
+
+    override val viewState: StateFlow<StepDetailViewState>
+        get() = stepDetailViewState
+
+    init {
+        processActions()
+    }
+
+    override fun processIntent(intents: Flow<StepDetailViewIntent>) {
+        intents.onEach { intent ->
+            actionsChannel.offer(stepDetailIntentProcessor.intentToAction(intent))
+        }.launchIn(viewModelScope)
+    }
+
+    private fun processActions() {
+        actionsChannel.asFlow()
+            .flatMapMerge { viewAction ->
+                stepDetailActionProcessor.actionToResult(viewAction)
+            }.scan(StepDetailViewState.Idle) { previous: StepDetailViewState, result ->
+                viewStateReducer.reduce(previous, result)
+            }.distinctUntilChanged()
+            .onEach { viewState ->
+                stepDetailViewState.value = viewState
+            }.launchIn(viewModelScope)
+    }
+
+    fun goBack() {
+        navigationDispatcher.goBack()
+    }
+}
